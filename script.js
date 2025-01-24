5435ab32070ef5a229e8f814c563f42e48b7ac52
Hugo <h.claveille@gmail.com>
2025-01-24 14:14:19 +0100

    sidebar sticky fonctionel

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// script.js
 var animation = lottie.loadAnimation({
     container: document.getElementById('lottie-logo'), // ID du conteneur où l'animation sera rendue
     renderer: 'svg',
@@ -11,26 +10,36 @@ var animation = lottie.loadAnimation({
 var lottieLogo = document.getElementById('lottie-logo');
 var isCross = false; // Variable pour suivre l'état actuel
 
-lottieLogo.addEventListener('click', function() {
+lottieLogo.addEventListener('click', function () {
     if (isCross) {
-        // Si l'état actuel est la croix, jouer l'animation pour revenir aux trois lignes
-        animation.goToAndPlay(animation.totalFrames / 2, true);
-        animation.addEventListener('enterFrame', function(event) {
-            if (event.currentTime >= animation.totalFrames) {
-                animation.goToAndStop(0, true);
-                animation.removeEventListener('enterFrame');
-            }
-        });
+        closeSideBar();
+        animation.goToAndPlay(animation.totalFrames / 2, true); // Jouer l'animation à partir de la moitié
         isCross = false;
     } else {
-        // Si l'état actuel est les trois lignes, jouer l'animation pour aller à la croix
-        animation.goToAndPlay(0, true);
-        animation.addEventListener('enterFrame', function(event) {
-            if (event.currentTime >= animation.totalFrames / 2) {
+        animation.goToAndPlay(0, true); // Jouer l'animation à partir de la première image
+        // var onEnterFrame = function (event) {
+        //     if (event.currentTime >= animation.totalFrames / 2) {
+        //         animation.removeEventListener('enterFrame', onEnterFrame);
+        //         animation.goToAndStop(animation.totalFrames / 2, true);
+        //     }
+        // };
+        // animation.addEventListener('enterFrame', onEnterFrame);
+        animation.addEventListener("enterFrame", function () {
+            if (animation.currentFrame >= animation.totalFrames / 2) {
+                animation.removeEventListener("enterFrame");
                 animation.goToAndStop(animation.totalFrames / 2, true);
-                animation.removeEventListener('enterFrame');
             }
         });
+        // ! ERREUR DANS LA CONSOLLE A CORRIGER
+        openSideBar();
         isCross = true;
     }
 });
+
+function openSideBar() {
+    document.getElementById("sidebar").style.width = "250px";
+}
+
+function closeSideBar() {
+    document.getElementById("sidebar").style.width = "50px";
+}
