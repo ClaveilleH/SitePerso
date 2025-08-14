034aa61c0be24309a197603883f0315dcee6e260
Hugo <h.claveille@gmail.com>
2025-08-14 17:25:36 +0200

    correction + ajout boutons login register

--- base/header.php
+++ base/header.php
@@ -1,7 +1,14 @@
 <?php include '../config.php'; ?>
+<link rel="stylesheet" href="/base/header.css">
 <header>
     <a href="<?php echo BASE_URL; ?>">
         <img id="logo" src="<?php echo $logoPath; ?>" alt="Logo de mon site web">
     </a>
-    <h1>Hugo CLAVEILLE</h1>
+    <div id="nom">
+        <h1>Hugo CLAVEILLE</h1>
+    </div>
+    <div id="buttons">
+        <button id="login">Connexion</button>
+        <button id="">Inscription</button>
+    </div>
 </header>
